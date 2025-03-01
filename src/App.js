dbd7f641c7af48646a11f0a9165f0762a9104dbf
emanuel-22 <emanuelbarboza5@gmail.com>
2025-03-01 19:44:29 -0300

    cambios hasta clase 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './platzi.webp';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+
 import './App.css';
 
 function App() {
   return (
+    // Esta es la estructura de componentes de App
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList >
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </TodoList>
+
+      {/* <CreateTodoButton/> */}
+      
     </div>
   );
 }
+
+
+
 
 export default App;
